5af0d9b41caa20785dc827946f0cf68aa417af50
Ulaş Gergerli <20200808049@ogr.akdeniz.edu.tr>
2023-12-08 20:44:45 +0300

    space soldier

--- Assets/AnimTester.cs
+++ Assets/AnimTester.cs
@@ -6,14 +6,12 @@ using UnityEngine;
 
 public class AnimTester : SoldierAnimator
 {
-    // Start is called before the first frame update
 
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
