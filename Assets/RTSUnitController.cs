1369a07b0498251705523dde6e065bf5f336f835
ozzlems <ozlemsencoruh@gmail.com>
2023-12-10 20:49:26 +0300

    Updated RTS unit controller script

--- Assets/RTSUnitController.cs
+++ Assets/RTSUnitController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RTSUnitController : MonoBehaviour
@@ -14,5 +15,58 @@ public class RTSUnitController : MonoBehaviour
     {
         selectedUnitList = new List<UnitController>();  
         UnitList = unitSpawner.SpawnUnits();    
+    }
+    public void ClickSelectUnit(UnitController newUnit)
+    {
+        DeselectAll();
+        SelectUnit(newUnit);
+    }
+    public void ShiftClickSelectUnit(UnitController newUnit)
+    {
+     if(selectedUnitList.Contains(newUnit))
+        {
+            DeselectUnit(newUnit);
+        }
+        else
+        {
+            SelectUnit(newUnit);
+        }
+    }   
+
+    public void DragSelectUnit(UnitController newUnit)
+    {
+        if(!selectedUnitList.Contains(newUnit))
+        {
+            SelectUnit(newUnit);
+        }   
+    }
+
+    public void MoveSelectedUnits(Vector3 end)
+    {
+        for(int i = 0; i < selectedUnitList.Count; i++)
+        {
+            selectedUnitList[i].Moveto(end);
+        }
+    }
+
+    public void DeselectAll()
+    {
+          for(int i = 0; i < selectedUnitList.Count; i++)
+        {
+            selectedUnitList[i].DeselectUnit();
+        }
+        selectedUnitList.Clear();   
+    }
+
+    private void SelectUnit(UnitController newUnit)
+    {
+      newUnit.SelectUnit();
+        selectedUnitList.Add(newUnit);
+    }
+    
+    private void DeselectUnit(UnitController newUnit)
+    {
+        newUnit.DeselectUnit();
+        selectedUnitList.Remove(newUnit);
     }
 }
