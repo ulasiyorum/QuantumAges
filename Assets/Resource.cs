d331ed5e6f673c3d04dff0d28ac6ddf5fc2b8698
ozzlems <ozlemsencoruh@gmail.com>
2023-12-14 13:00:32 +0300

    added Resource script

---
+++ Assets/Resource.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resource : MonoBehaviour
+{
+    
+  public float amount = 100f;
+    public int resourceType;
+    public GameObject[] collection_points;
+
+    private bool[] collection_points_usage;
+
+    private void Start()
+    {
+        collection_points_usage = new bool[collection_points.Length];
+        for (int i = 0; i < collection_points.Length; i++)
+        {
+            collection_points_usage[i] = false;
+        }
+    }
+
+    private void Update()
+    {
+        
+    }
+    public bool collectResource()
+    {
+        for (int i = 0; i < collection_points_usage.Length; i++)
+        {
+            if (!collection_points_usage[i])
+            {
+                collection_points_usage[i] = true;  
+                return true;
+            }
+        }
+        return false;
+    }   
+
+}
+
+
+
+
+
+
